1d163c86a3a4f2f764cfc7917215c4d305449520
Surowiec Krzysztof <krzysztof.surowiec@volvo.com>
2021-08-08 20:58:23 +0200

    create the sqlalchemy engine and first model for blog

---
+++ app/blog/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from . import schemas, models
+from .database import engine
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+
+@app.post('/blog')
+def create(request: schemas.Blog):
+    return request
--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,3 +19,35 @@ def about():
     return {
         'data': 'about page'
     }
+
+
+@app.get('/blog/unpublished')
+def unpublished_blogs():
+    return {'data': 'all unpublished blogs'}
+
+
+@app.get('/blogs')
+def get_blogs(limit: int = 10, published: bool = True):
+    if published:
+        return {'data': 'return all published blogs'}
+
+    return {'data': 'return all blogs'}
+
+
+@app.get('/blog/{id}')
+def get_blog(id: int):
+    return {'data': id}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post('/blog')
+def create_blog(request: Blog):
+    return {'data': f'Blog is created with {request.title}'}
+
+
+
